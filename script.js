571ff68aac9c6e582af35bdf0ee30d1ebf7434a3
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2021-11-18 17:33:55 -0300

    Layout-tela 2 feito, falta sortear e uns detalhes

--- script.js
+++ script.js
@@ -11,24 +11,57 @@ solicitarQuizzes.then(quizzesPag1)
 function quizzesPag1(resposta) {
   /* let quizz = carregarQuizzes() */
   let quizz = resposta.data
-  console.log(quizz)
   let containerQuizzes = document.querySelector('ul')
-  containerQuizzes.innerHTML = `<li class="quizz" onclick="quizzTela2()">
-  <p class="nome-quizz">${quizz[0].title}</p>
-  </li>
-  <style>
-  .quizz {
-    background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${quizz[0].image});
-  }</style>`
+  for (let i = 0; i < quizz.length; i++) {
+    containerQuizzes.innerHTML += `<li style="background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${quizz[i].image})" class="quizz" onclick="infoQuizz()">
+    <p class="nome-quizz">${quizz[i].title}</p>
+    </li>`
+  }
 }
 
-function quizzTela2() {
-  const carregarQuizzes = axios.get(
-    'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes'
+function infoQuizz() {
+  const teste = axios.get(
+    'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1'
   )
-  carregarQuizzes.then
-  const fecharTela1 = document.querySelector('.seus-quizzes')
+  teste.then(quizzTela2)
+}
+function quizzTela2(resposta) {
+  const infoQuizz = resposta.data
+  const fecharTela1 = document.querySelector('.tela1')
   fecharTela1.classList.add('display-none')
   const abrirTela2 = document.querySelector('.tela2')
   abrirTela2.classList.remove('display-none')
+
+  const cabecalhoTela2 = document.querySelector('.cabecalho-tela2')
+  cabecalhoTela2.innerHTML = `<div class="img-tela2">
+    <p class="nome-quizz-2">${infoQuizz.title}</p>
+    </div>
+    <style>
+    .img-tela2 {
+      background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${infoQuizz.image});
+    }</style>`
+
+  const caixaPergunta = document.querySelector('.caixa-perguntas')
+  for (let i = 0; i < infoQuizz.questions.length; i++) {
+    caixaPergunta.innerHTML += `<div class="pergunta">
+    <div style="background-color: ${infoQuizz.questions[i].color}" class="titulo-pergunta">${infoQuizz.questions[i].title}</div>
+    <div class="todasRespostas"></div>
+    </div>`
+
+    const tdsRespostas = document.querySelectorAll('.todasRespostas')
+    for (let j = 0; j < infoQuizz.questions[i].answers.length; j++) {
+      console.log(infoQuizz.questions[i].answers[j].text)
+      tdsRespostas[i].innerHTML += `<div class="opcao-resposta">
+          <img class="img-pergunta" src="${infoQuizz.questions[i].answers[j].image}"></img>
+          <p class="opcao-pergunta">${infoQuizz.questions[i].answers[j].text}</p>
+        </div>
+        `
+    }
+  }
+
+  /* const adicionaCaixa = document.querySelector('pergunta')
+  const adicionaPergunta = document.querySelector('.caixa-pergunta')
+  for (let x = 0; x < infoQuizz.questions.length; x++) {
+    adicionaPergunta.innerHTML = tituloPergunta + opcoesResposta
+  } */
 }
