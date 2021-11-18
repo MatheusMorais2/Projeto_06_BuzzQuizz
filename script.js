7801263876033c6844f8acbc9fd97b9ec4dd5952
Matheus <morais.matheus.web@gmail.com>
2021-11-18 17:50:13 -0300

    Logica das respostas e layout do quadro de resultado

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ function quizzesPag1(resposta) {
   let quizz = resposta.data
   console.log(quizz)
   let containerQuizzes = document.querySelector('ul')
-  containerQuizzes.innerHTML = `<li class="quizz" onclick="quizzTela2()">
+  /* containerQuizzes.innerHTML = `<li class="quizz" onclick="quizzTela2()">
   <p class="nome-quizz">${quizz[0].title}</p>
   </li>
   <style>
   .quizz {
     background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${quizz[0].image});
-  }</style>`
+  }</style>` */
 }
 
 function quizzTela2() {
@@ -32,3 +32,22 @@ function quizzTela2() {
   const abrirTela2 = document.querySelector('.tela2')
   abrirTela2.classList.remove('display-none')
 }
+
+
+// COMPORTAMENTO DAS RESPOSTAS
+const respostaCorretaQuiz = 'Abacate';
+function selecionarResposta(respEscolhida) {
+  const listaOpcoesRespostas = respEscolhida.parentNode;
+  const todasRespostas = listaOpcoesRespostas.children;
+  if (respEscolhida.innerText === respostaCorretaQuiz) {
+    respEscolhida.classList.add('resposta-correta'); 
+  } else {
+    respEscolhida.classList.add('resposta-errada'); 
+  }
+  for (let i=0; i<todasRespostas.length; i++) {
+    todasRespostas[i].innerHTML+= `<div class='esbranquicado'></div>`;
+    todasRespostas[i].removeAttribute('onclick');
+  }
+  respEscolhida.querySelector('.esbranquicado').remove();
+  //setTimeout( pegar a proxima irma da div da pergunta,2000)
+} 
